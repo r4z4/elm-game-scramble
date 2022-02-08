0bdca8e671efac51f7524e90b3dfe3cccb6fb63d
r4z4 <85771218+r4z4@users.noreply.github.com>
2022-02-07 20:57:43 -0600

    Add files via upload

---
+++ precache-manifest.58f501495c818850732495068d688127.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "675c751ac03410762c085ccf3b14891f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "ef8e741c666a7ce0c13f",
+    "url": "/static/css/main.454bbf59.chunk.css"
+  },
+  {
+    "revision": "ef8e741c666a7ce0c13f",
+    "url": "/static/js/main.ef8e741c.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/static/js/vendors~main.354e092f.chunk.js"
+  },
+  {
+    "revision": "6d46cbc6ecb9cbe5aa1c9aad8ee21a77",
+    "url": "/test.png"
+  }
+]);
